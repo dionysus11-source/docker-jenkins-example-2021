8d95bd0dcf72a75f192be38f2752d959d68be6af
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2022-01-01 16:26:07 +0900

    add

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: 'mysql',
-    user : 'root',
-    password : 'password',
-    database : 'myapp',
-    port : 3306
-})
-exports.pool = pool
+    user: 'root',
+    password: 'johnahn',
+    database: 'myapp',
+    port: 3306
+});
+exports.pool = pool;
